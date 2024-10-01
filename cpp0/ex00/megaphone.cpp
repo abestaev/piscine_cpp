c0cd8bd1ad7e18a1f61b2e1f5cc28a998876c800
abestaev <gatsby.hewitt@gmail.com>
2024-10-01 03:09:55 +0200

    cpp0/ex00

---
+++ cpp0/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+void megaphone(char *str)
+{
+    for (int i = 0; str[i]; i++)
+        std::cout << (char)toupper(str[i]);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+    {
+        for (int i = 1; i < argc; i++)
+            megaphone(argv[i]);
+    }
+    std::cout << std::endl;
+    return 0;
+} 
